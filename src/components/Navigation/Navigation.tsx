646020dd7f0e7b6a1e42e31906c9a80b913518fd
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-11-14 00:45:16 +0500

    Navbar updated

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import ThemeSwitch from '../ThemeSwitch/ThemeSwitch'
+import React from "react";
+import ThemeSwitch from "../ThemeSwitch/ThemeSwitch";
+import Images from "next/image";
+import Logo from "@/assets/logo/env-byte-logo.svg";
 
 function Navigation() {
   return (
     <>
-    <nav className="flex sm:justify-center space-x-4">
-        {[
-            ['Home', '/'],
-            ['Version 1', '/version1'],
-        ].map(([title, url]) => (
-            <a key={title} href={url} className="rounded-lg px-3 py-2 font-semibold text-slate-800 hover:bg-slate-700 hover:text-slate-100 dark:text-slate-100 dark:hover:text-slate-800 dark:hover:bg-zinc-300">{title}</a>
-        ))}
+      <nav className="flex items-center space-x-4 sm:justify-center">
+        <Images
+          src={Logo}
+          alt="Env Byte Logo"
+          className="w-28"
+          width={1000}
+          height={1000}
+        />
         <ThemeSwitch />
-    </nav>
+      </nav>
     </>
-  )
+  );
 }
 
-export default Navigation
+export default Navigation;
