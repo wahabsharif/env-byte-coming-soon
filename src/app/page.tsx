9ceb3c33ab03205b24c2a186da7237accac0f1b1
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-11-14 00:37:23 +0500

    First Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import StandardComingSoon from "@/components/StandardComingSoon/StandardComingSoon";
+import ComingSoonVersion1 from "@/components/ComingSoonVersion1/ComingSoonVersion1";
+
+export default function Home() {  
+  return (    
+      <>       
+        {/* Standard Version */}
+        <StandardComingSoon />
+      </>
+  );
+}
