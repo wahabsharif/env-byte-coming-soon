a4d83af8cf87a8e2977253a8ad77628a5e1ae3d0
Muhammad Wahab Sharif <www.rex19@gmail.com>
2024-11-14 22:13:07 +0500

    icon path fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="favicon.ico" />
       </Head>
       <body
         className={`${inter.className} bg-gray-200	min-h-screen dark:bg-[#0d1117] `}
